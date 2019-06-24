6ee6cea9f9c8f7010154671ffa057df69918bf15
Lucky Amadi <lucky@softcom.ng>
2019-06-24 22:14:35 +0100

    started working on the headline

---
+++ src/News/Headline.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Card, ListItem, Button, Icon } from "react-native-elements";
+
+const Headline = () => {
+	return (
+		<View>
+			<Card title="CARD WITH DIVIDER">
+				return (
+				<View key={i}>
+					<Image resizeMode="cover" />
+					<Text> Text</Text>
+				</View>
+				);
+			</Card>
+		</View>
+	);
+};
+
+export default Headline;
