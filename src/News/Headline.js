6d8ae9b38a22ea9b1dd076c2f7f7b10fb8a081d0
Lucky Amadi <lucky@softcom.ng>
2019-06-29 22:39:43 +0100

    added the headline

--- src/News/Headline.js
+++ src/News/Headline.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Card } from "react-native-elements";
 
 const Headline = () => {
 	return (
 		<View>
-			<View style={styles.container}>
-				<Text>Title</Text>
-			</View>
+			{/* <View style={styles.container}>
+				<Text style={styles.title}>Title</Text>
+			</View> */}
+			<Card title="Trending">
+				<Image
+					source={{
+						uri: "https://www.dailynews.com/wp-content/uploads/2017/09/img_3776.jpg"
+					}}
+					style={{ height: 200, width: 300 }}
+				/>
+				<Text style={{ marginBottom: 10 }}>
+					The idea with React Native Elements is more about component structure than actual design.
+				</Text>
+			</Card>
 		</View>
 	);
 };
@@ -18,6 +30,9 @@ const styles = StyleSheet.create({
 		borderWidth: 0.5,
 		borderColor: "#d6d7da",
 		height: 150
+	},
+	title: {
+		marginBottom: 0
 	}
 });
 
