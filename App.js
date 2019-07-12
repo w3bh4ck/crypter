6017e9bfec0425bd02064ae5fae0727df96e24de
Lucky Amadi <lucky@softcom.ng>
2019-07-12 23:53:10 +0100

    added ui kitten

--- App.js
+++ App.js
@@ -7,25 +7,23 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import { ThemeProvider } from "react-native-elements";
-import Layout from "./src/components/Layout";
+// import { Platform, StyleSheet, Text, View } from "react-native";
+// import { ThemeProvider } from "react-native-elements";
+// import Layout from "./src/components/Layout";
+import { mapping, light as lightTheme } from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "react-native-ui-kitten";
+import Navigation from "./src/components/Navigation";
 
-const instructions = Platform.select({
-	ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-	android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
-});
+// const instructions = Platform.select({
+// 	ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+// 	android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
+// });
 
-class App extends Component {
-	render() {
-		return (
-			<View>
-				<ThemeProvider>
-					<Layout />
-				</ThemeProvider>
-			</View>
-		);
-	}
-}
+const App = () => (
+	<ApplicationProvider mapping={mapping} theme={lightTheme}>
+		<Navigation title="Crypto" />
+		<Layout style={{ flex: 1 }} />
+	</ApplicationProvider>
+);
 
 export default App;
