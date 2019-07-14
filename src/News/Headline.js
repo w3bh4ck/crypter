ee5dcee9246823bb9c5ad076e0cc1fbdf462e5b8
Lucky Amadi <lucky@softcom.ng>
2019-07-14 23:19:18 +0100

    fixing bugs

--- src/News/Headline.js
+++ src/News/Headline.js
@@ -6,12 +6,12 @@ const Headline = () => {
 	return (
 		<View>
 			<View style={styles.container}>
-				<Image
+				{/* <Image
 					style={{ width: "100%", height: "100%" }}
 					resizeMode={"cover"}
 					source={{ uri: "https://www.dailynews.com/wp-content/uploads/2017/09/img_3776.jpg" }}
 					style={styles.trendingImage}
-				/>
+				/> */}
 			</View>
 		</View>
 	);
